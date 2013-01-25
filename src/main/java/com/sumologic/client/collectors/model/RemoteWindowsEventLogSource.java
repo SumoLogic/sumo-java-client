0edd22f17f6337d21950954bd814988ef59b3e90
Jose Muniz <jose@sumologic.com>
2013-01-25 11:22:26 -0800

    SUMO-12823 Add logNames field to Windows sources

--- src/main/java/com/sumologic/client/collectors/model/RemoteWindowsEventLogSource.java
+++ src/main/java/com/sumologic/client/collectors/model/RemoteWindowsEventLogSource.java
@@ -13,6 +13,7 @@ public class RemoteWindowsEventLogSource extends WindowsEventLogSource {
     private static String USERNAME = "username";
     private static String PASSWORD = "password";
     private static String HOSTS = "hosts";
+    private static String LOG_NAMES = "logNames";
 
     public RemoteWindowsEventLogSource() {
         setSourceType(SourceType.REMOTE_WINDOWS_EVENT_LOG.getType());
@@ -81,4 +82,21 @@ public class RemoteWindowsEventLogSource extends WindowsEventLogSource {
     public void setHosts(List<String> hosts) {
         setProperty(HOSTS, hosts);
     }
+
+    /**
+     * Returns the list of event types (empty if includeAll = true).
+     *
+     * @return The list of event types.
+     */
+    public List<String> getLogNames() {
+        return getProperty(LOG_NAMES);
+    }
+
+    /**
+     * Sets the list of event types.
+     */
+    public void setLogNames(List<String> logNames) {
+        setProperty(LOG_NAMES, logNames);
+    }
+
 }
