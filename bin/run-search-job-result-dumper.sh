c98892193e26126dedab66a289eba8f8c6a522d7
Greg Oledzki <goledzki@sumologic.com>
2021-08-06 09:25:26 +0200

    Upgrading dependencies

--- bin/run-search-job-result-dumper.sh
+++ bin/run-search-job-result-dumper.sh
@@ -1,1 +1,1 @@
-java -cp "target/sumo-java-client-1.1-SNAPSHOT-jar-with-dependencies.jar" -server -Xmx128m com.sumologic.client.searchjob.SearchJobResultDumper $*
+java -cp "target/sumo-java-client-2.8-SNAPSHOT-jar-with-dependencies.jar" -server -Xmx128m com.sumologic.client.searchjob.SearchJobResultDumper $*
